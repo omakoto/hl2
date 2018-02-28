42d500a184e9a4863c90a7d9e69b6dca49049dc0
Makoto Onuki <omakoto@google.com>
2018-02-27 21:31:32 -0800

    latest

--- src/hl/textio/linereader.go
+++ src/hl/textio/linereader.go
@@ -2,10 +2,12 @@ package textio
 
 import (
 	"bufio"
+	"github.com/omakoto/hl2/src/hl/util"
 	"github.com/pborman/getopt/v2"
 	"io"
 )
 
+// LineReader is a wrapper around io.Reader that supports read line by line, optionally supporting CR as a line terminator in addition to LF.
 type LineReader interface {
 	ReadLine() (line []byte, err error)
 }
@@ -41,6 +43,7 @@ type lineReader struct {
 	line []byte
 }
 
+// NewLineReader creates a new LineReader.
 func NewLineReader(r io.Reader, crAware bool) LineReader {
 	if crAware {
 		return &lineReader{
@@ -65,7 +68,7 @@ func findCrOrLf(data []byte, start, end int) int {
 
 func (l *lineReader) ReadLine() ([]byte, error) {
 	for {
-		//util.Debugf("ReadLine: %d / %d\n", l.next, l.avail)
+		util.Debugf("ReadLine: %d / %d\n", l.next, l.avail)
 		if l.next >= l.avail {
 			if l.err != nil {
 				return nil, l.err
@@ -74,7 +77,7 @@ func (l *lineReader) ReadLine() ([]byte, error) {
 			l.next = 0
 
 			l.avail, l.err = l.reader.Read(l.buf)
-			//util.Debugf("Read: %d byte(s) read, err=%v, data=\"%s\"\n", l.avail, l.err, string(l.buf))
+			util.Debugf("Read: %d byte(s) read, err=%v, data=\"%s\"\n", l.avail, l.err, l.buf)
 			if l.avail == 0 {
 				if len(l.line) > 0 {
 					return l.line, l.err
@@ -84,22 +87,22 @@ func (l *lineReader) ReadLine() ([]byte, error) {
 		}
 		start := l.next
 		lfPos := findCrOrLf(l.buf, start, l.avail)
-		//util.Debugf("Next segment: %d - %d\n", start, lfPos)
+		util.Debugf("Next segment: %d - %d\n", start, lfPos)
 		if lfPos >= 0 {
 			l.next = lfPos + 1
 			if len(l.line) > 0 {
-				l.line = append(l.line, l.buf[start:lfPos]...)
+				l.line = append(l.line, l.buf[start:l.next]...)
 				ret := l.line
-				//util.Debugf("Return: \"%s\"\n", string(ret))
+				util.Debugf("Return: \"%s\"\n", string(ret))
 				l.line = l.line[0:0]
 				return ret, nil
 			}
-			ret := l.buf[start : lfPos+1]
-			//util.Debugf("Return: \"%s\"\n", string(ret))
+			ret := l.buf[start:l.next]
+			util.Debugf("Return: \"%s\"\n", string(ret))
 			return ret, nil
 		}
 		l.next = l.avail
 		l.line = append(l.line, l.buf[start:l.avail]...)
-		//util.Debugf("Line buffer: \"%s\"\n", string(l.line))
+		util.Debugf("Line buffer: \"%s\"\n", l.line)
 	}
 }
