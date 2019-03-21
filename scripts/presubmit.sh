90944cd5130858d2aa86abdd849ea15436e81ed0
Makoto Onuki <makoto.onuki@google.com>
2019-03-20 21:41:42 -0700

    Clean up

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -4,16 +4,13 @@ set -e
 
 . "${0%/*}"/build.sh
 
-#go get github.com/golang/lint/golint                        # Linter
-#go get honnef.co/go/tools/cmd/megacheck                     # Badass static analyzer/linter
-
 gofmt -s -d $(find . -type f -name '*.go') |& perl -pe 'END{exit($. > 0 ? 1 : 0)}'
 
-go test -v -race ./...                   # Run all the tests with the race detector enabled
+go test -v -race ./...
 ./tests/testall.sh
 
-go vet ./...                             # go vet is the official Go static analyzer
-megacheck ./...                          # "go vet on steroids" + linter
+go vet ./...
+staticcheck ./...
 golint $(go list ./...) |& grep -v '\(exported .* should have\|comment on exported\)' | perl -pe 'END{exit($. > 0 ? 1 : 0)}'
 
 echo "All check passed."
