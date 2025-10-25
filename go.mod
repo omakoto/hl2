458deafee82ea371224158dd5936f79bde8c98b8
Makoto Onuki <makoto.onuki@gmail.com>
2025-10-25 11:11:39 -0700

    Revert "Switch pcre impl to golang native one"
    This reverts commit be9466b4a154f9706cdc3b46d29dabf85fbe8c3e.

--- go.mod
+++ go.mod
@@ -4,22 +4,18 @@ go 1.20
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/d4l3k/go-pcre v0.0.0-20160928060324-cf2c5e16ca45
 	github.com/davecgh/go-spew v1.1.1
 	github.com/mattn/go-isatty v0.0.19
 	github.com/omakoto/go-common v0.0.0-20230902054104-3c406b670d93
 	github.com/pborman/getopt/v2 v2.1.0
 	github.com/stretchr/testify v1.8.4
-	go.elara.ws/pcre v0.0.0-20230805032557-4ce849193f64
 	golang.org/x/term v0.11.0
 )
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	modernc.org/libc v1.16.8 // indirect
-	modernc.org/mathutil v1.4.1 // indirect
-	modernc.org/memory v1.1.1 // indirect
 )
--- src/hl/matcher/regex_pcre.go
+++ src/hl/matcher/regex_pcre.go
@@ -1,7 +1,8 @@
 package matcher
 
 import (
-	"go.elara.ws/pcre"
+	"fmt"
+	"github.com/d4l3k/go-pcre"
 )
 
 type matcherPcre struct {
@@ -25,39 +26,72 @@ func CompilePcre(pattern string, flags Flags) (Matcher, error) {
 	if err != nil {
 		return nil, err
 	}
-	pcreFlags := pcre.UTF | pcre.NoUTFCheck
+	pcreFlags := pcre.UTF8 | pcre.NO_UTF8_CHECK
 	if (flags & IgnoreCase) != 0 {
-		pcreFlags |= pcre.Caseless
+		pcreFlags |= pcre.CASELESS
 	}
-	pat, err := pcre.CompileOpts(realPattern, pcreFlags)
+	pat, err := pcre.Compile(realPattern, pcreFlags)
 	if err != nil {
 		return nil, err
 	}
 
-	return &matcherPcre{srcPattern: pattern, realPattern: realPattern, negate: negate, pattern: pat}, nil
+	return &matcherPcre{srcPattern: pattern, realPattern: realPattern, negate: negate, pattern: &pat}, nil
 }
 
 func (r *matcherPcre) Matches(target []byte) [][]int {
 	if r.negate {
-		if !r.pattern.Match(target) {
+		if !r.pattern.Matcher(target, 0).Matches() {
 			return [][]int{{0, len(target)}}
 		}
 		return nil
 	}
-	matches := r.pattern.FindAllSubmatchIndex(target, -1)
-	if len(matches) == 0 {
+	m := r.pattern.NewMatcher()
+
+	res := make([][]int, 0)
+
+	start := 0
+	for {
+		if start > len(target) {
+			break
+		}
+		//util.Debugf("Start=%d\n", start)
+		//util.Debugf("  Target=\"%s\"\n", string(target[start:]))
+		result := m.Exec(target[start:], 0)
+		if result == pcre.ERROR_NOMATCH || result == pcre.ERROR_BADUTF8 {
+			break
+		}
+		if result < 0 {
+			panic(fmt.Sprintf("pcre_exec returned %d for pattern \"%s\" (len %db), target \"%s\", index %d", result, r.realPattern, len([]byte(r.realPattern)), string(target), start))
+		}
+		// matchStart := m.GroupIndices(0)[0]
+		matchEnd := m.GroupIndices(0)[1] + start
+		//util.Debugf("  Match=%d, %d\n", matchStart, matchEnd)
+
+		groups := m.Groups()
+		if groups == 0 {
+			indexes := make([]int, 2)
+			indexes[0] = m.GroupIndices(0)[0] + start
+			indexes[1] = m.GroupIndices(0)[1] + start
+			res = append(res, indexes)
+		} else {
+			for j := 1; j <= groups; j++ {
+				gi := m.GroupIndices(j)
+				if gi != nil {
+					indexes := make([]int, 2)
+					indexes[0] = gi[0] + start
+					indexes[1] = gi[1] + start
+					res = append(res, indexes)
+				}
+			}
+		}
+
+		if matchEnd == start {
+			matchEnd++
+		}
+		start = matchEnd
+	}
+	if len(res) == 0 {
 		return nil
 	}
-	captures := (len(matches[0]) / 2) - 1
-	if captures == 0 {
-		return matches
-	}
-
-	ret := make([][]int, 0, captures*len(matches))
-	for i := 0; i < len(matches); i++ {
-		for j := 0; j < captures; j++ {
-			ret = append(ret, []int{matches[i][2+j*2], matches[i][2+j*2+1]})
-		}
-	}
-	return ret
+	return res
 }
